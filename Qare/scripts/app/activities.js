e9f896660f2d837c968b7e26aacd5e1dd339754d
Developer Qare <tony@qare.nl>
2015-09-24 19:00:06 +0000

    bug maps fixed

--- Qare/scripts/app/activities.js
+++ Qare/scripts/app/activities.js
@@ -22,17 +22,17 @@ app.showMap = (function(position) {
 
     };
 
-    var map = new google.maps.Map(
-    $('#map'),
-    mapOptions
-    );
+    //var map = new google.maps.Map(
+    //$('#map'),
+    //mapOptions
+    //);
 
-    var marker = new google.maps.Marker({
-    	position: latlng,
-    	map: map
-    });
-		console.log(marker);
-		console.log("map rendering");
+    //var marker = new google.maps.Marker({
+    //	position: latlng,
+    //	map: map
+    //});
+	//	console.log(marker);
+	//	console.log("map rendering");
 
  
 
@@ -215,7 +215,7 @@ app.activitiesReload = (function () {
     // app.Activities.activities.group(
     //     { field: "formattedActivityDate" })
     console.log("activities reloaded " +  app.Activities.activities);
-    navigator.geolocation.getCurrentPosition(app.showMap);
+//    navigator.geolocation.getCurrentPosition(app.showMap);
 });
 app.init = (function () {
      console.log("activities init");
